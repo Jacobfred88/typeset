38992a98b1269522c12567ee83ebc8acf85c7d86
Jacob Frederiksen <jacobfred88@gmail.com>
2018-11-15 20:08:43 +0100

    add type settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hello world</h1>
+        <p>The next shoes to drop seem likely clear: GOP operative Roger Stone has long suspected he'll be indicted. Stone ally Jerome Corsi suggested in a recent YouTube livestream that he may face charges as well. Even Donald Trump's son, Donald Jr., has reportedly discussed with friends the possibility of his own indictment. At the same time, the forced resignation of attorney general Jeff Sessions, and appointment of his chief of staff Matt Whitaker to be the acting attorney general—a decision that is of at least debatable legality—has raised fears that the Trump administration is closing in on firing Mueller himself.</p>
+        <h2>Hello world</h2>
+        <p>The next shoes to drop seem likely clear: GOP operative Roger Stone has long suspected he'll be indicted. Stone ally Jerome Corsi suggested in a recent YouTube livestream that he may face charges as well. Even Donald Trump's son, Donald Jr., has reportedly discussed with friends the possibility of his own indictment. At the same time, the forced resignation of attorney general Jeff Sessions, and appointment of his chief of staff Matt Whitaker to be the acting attorney general—a decision that is of at least debatable legality—has raised fears that the Trump administration is closing in on firing Mueller himself.</p>
+        <h3>Hello world</h3>
+        <p>The next shoes to drop seem likely clear: GOP operative Roger Stone has long suspected he'll be indicted. Stone ally Jerome Corsi suggested in a recent YouTube livestream that he may face charges as well. Even Donald Trump's son, Donald Jr., has reportedly discussed with friends the possibility of his own indictment. At the same time, the forced resignation of attorney general Jeff Sessions, and appointment of his chief of staff Matt Whitaker to be the acting attorney general—a decision that is of at least debatable legality—has raised fears that the Trump administration is closing in on firing Mueller himself.</p>
+        <h4>Hello world</h4>
+        <p>The next shoes to drop seem likely clear: GOP operative Roger Stone has long suspected he'll be indicted. Stone ally Jerome Corsi suggested in a recent YouTube livestream that he may face charges as well. Even Donald Trump's son, Donald Jr., has reportedly discussed with friends the possibility of his own indictment. At the same time, the forced resignation of attorney general Jeff Sessions, and appointment of his chief of staff Matt Whitaker to be the acting attorney general—a decision that is of at least debatable legality—has raised fears that the Trump administration is closing in on firing Mueller himself.</p>
+
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic quia illo optio error doloremque necessitatibus magni quas iusto repellat repudiandae veritatis, cupiditate, libero illum aperiam voluptates deleniti ex corporis expedita.</h1>
+        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic quia illo optio error doloremque necessitatibus magni quas iusto repellat repudiandae veritatis, cupiditate, libero illum aperiam voluptates deleniti ex corporis expedita.</h2>
+        <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic quia illo optio error doloremque necessitatibus magni quas iusto repellat repudiandae veritatis, cupiditate, libero illum aperiam voluptates deleniti ex corporis expedita.</h3>
+        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic quia illo optio error doloremque necessitatibus magni quas iusto repellat repudiandae veritatis, cupiditate, libero illum aperiam voluptates deleniti ex corporis expedita.</h4>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error est cum odit quos. Officia consequatur ratione, pariatur aliquam autem numquam similique blanditiis dignissimos doloremque facere ex nostrum perspiciatis, nesciunt neque.
+        Assumenda quisquam aspernatur, officia quo, maxime at reiciendis consectetur provident sit laboriosam laudantium natus ullam sed optio ratione nulla nostrum sunt hic inventore molestiae! Amet nesciunt molestias tempora consequatur non?
+        At itaque minima eum ullam facilis veniam excepturi commodi doloremque deserunt aspernatur rem similique alias quasi cupiditate, aliquid quo dicta fuga tenetur. Tenetur, architecto. Sint nam eos suscipit adipisci sunt.
+        Illo, rerum ducimus facilis cumque, officiis maxime velit minima suscipit enim nemo asperiores veniam deleniti praesentium sunt, illum numquam recusandae quibusdam architecto quas. Deserunt, iure! Cumque aliquid eum distinctio consequatur.
+        Labore mollitia delectus, non voluptas aut alias exercitationem aliquam voluptatum, ea id quaerat, error corporis laboriosam eos tempora facilis cum minus magni debitis modi esse temporibus sapiente. Eos, consectetur recusandae?
+        At aperiam voluptate voluptatum reiciendis aut natus iure hic quas deserunt? Nihil doloremque ipsa, perspiciatis similique non saepe facere velit numquam molestias placeat, facilis minus in odio. Quisquam, dolores inventore.</p>
       </div>
     );
   }
