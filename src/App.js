215ec2dfc03009e9f5f44db9a11c33841e7945ef
Jacob Frederiksen <jacobfred88@gmail.com>
2018-11-19 22:50:12 +0100

    add type

--- src/App.js
+++ src/App.js
@@ -5,6 +5,39 @@ class App extends Component {
     return (
         <div className="pageMargins">
           <div className="row">
+            <div className="col">
+              <h1 className="h1 h4-lg">Equilateral Triangle of a Perfect Paragraph</h1>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-4 col-lg-2">
+              <h1>Equilateral Triangle of a Perfect Paragraph</h1>
+
+              <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+              <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+
+              <h2>Font size is represented by the left side of the triangle </h2>
+
+              <ol>
+                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li>Aliquam tincidunt mauris eu risus.</li>
+              </ol>
+
+              <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+             
+              <hr />
+
+              <h3>Changes the width of the bottom size equally</h3>
+              <ul>
+                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li>Aliquam tincidunt mauris eu risus.</li>
+              </ul>
+              <h4>A Guide to Combining Fonts</h4>
+              <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+            </div>
+          </div>
+
+          {/* <div className="row">
             <div className="col-2 flex-align-end">
               <h4>Cüneyt Sepetçi ( TR )</h4>
             </div>
@@ -28,8 +61,8 @@ class App extends Component {
             </div>
             <div className="col-3">
               <h2>Thomas Williams is an Australian designer with over 12 years experience working across brand identity, publishing and digital products. </h2>
-            </div>
-          </header>
+            </div> */}
+          {/* </header> */}
           {/*
           <hr />
           <div class="row">
