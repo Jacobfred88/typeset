b8ceda055c0c9e9ddc4422a6028135e37c71ec19
Jacob Frederiksen <jacobfred88@gmail.com>
2018-12-03 14:09:08 +0100

    add new content

--- src/App.js
+++ src/App.js
@@ -2,24 +2,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-        <div className="pageMargins">
+        <div className="pageMargins grid">
           <div className="row justify-content-center">
-            <div className="col">
-              <h1 className="f6">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo tempore quasi eos esse, consequatur qui itaque magni non repudiandae fugit quia iure inventore odio commodi optio. Accusamus, libero error! Molestiae?</p>
-            </div>
-            <div className="col grid">
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit consectetur</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo tempore quasi eos esse, consequatur qui itaque magni non repudiandae fugit quia iure inventore odio commodi optio. Accusamus, libero error! Molestiae?</p>
-              <button>Hello</button>
-              <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit consectetur</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo tempore quasi eos esse, consequatur qui itaque magni non repudiandae fugit quia iure inventore odio commodi optio. Accusamus, libero error! Molestiae?</p>
-              <button>Hello</button>
-              <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit consectetur</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo tempore quasi eos esse, consequatur qui itaque magni non repudiandae fugit quia iure inventore odio commodi optio. Accusamus, libero error! Molestiae?</p>
-              <button>Hello</button>
-            </div>
-            {/* <div className="col-4 col-lg-2">
+          
+            <div className="col-4 col-lg-2">
 
               <h2 className="test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate enim eos magnam recusandae adipisci eligendi alias dolor modi dicta. Repellendus dolorem vero natus magnam nam earum dicta ab iusto quo!</h2>
               <h1>Equilateral Triangle of a Perfect Paragraph</h1>
@@ -51,116 +37,8 @@ class App extends Component {
               <h4>Equilateral Triangle of a Perfect Paragraph</h4>
               <h5>Equilateral Triangle of a Perfect Paragraph</h5>
               <h6>Equilateral Triangle of a Perfect Paragraph</h6>
-          </div> */}
+          </div> 
         </div>
-
-          {/* <div className="row">
-            <div className="col-2 flex-align-end">
-              <h4>Cüneyt Sepetçi ( TR )</h4>
-            </div>
-            <div className="col-1 text-r">
-              <p>Istanbul’s traditionally informed clarinet virtuoso Cüneyt Sepetçi is on an intense trip into modern Turkish wedding and party music. After mashing up the place at Roskilde Festival 2014 with the full Dolapdere wedding orchestra, Cüneyt will be back in November 2018 for his first Euro tour</p>
-            </div>
-            <div className="col-1">
-            <p>Lørdag d. 17. november<br />
-
-              Dørene åbner: kl. 20:00<br />
-
-              Koncerten starter: kl. 21:00<br />
-
-              Pris: 160 dkk inkl. gebyr</p>
-            </div>
-          </div>
-
-           <header className="row">
-            <div className="col-1">
-              <p>Digital design</p>
-            </div>
-            <div className="col-3">
-              <h2>Thomas Williams is an Australian designer with over 12 years experience working across brand identity, publishing and digital products. </h2>
-            </div> */}
-          {/* </header> */}
-          {/*
-          <hr />
-          <div class="row">
-            <div className="col-1 offset-1">
-              <p>He has worked and collaborated with industry leaders and global brands including Apple, Patagonia, MINI, Xero, and Sonos. Thomas’ work has been awarded and recognized globally and can be found in publications by Thames &amp; Hudson, Unit Editions, and Gestalten. He has lectured at numerous Universities around Australia and delivered keynotes at conferences including Semi-Permanent, The Design Conference and Creative Mornings.</p>
-            </div>
-            <div className="col-1">
-              <p>In 2011 he founded Made Publishers, a boutique publishing company with a focus on design and culture. Made Publishers has published over 20 editions of Process Journal, MADE and Nourished Journal, all of which can be found in stockists globally, including MoMA (New York) and The Design Museum (London).</p>
-              
-            </div>
-            <div className="col-1">
-              <p>In 2018 Thomas also launched Construct, a new digital product that creates responsive, web-based fee proposals. He spent over two years designing almost every aspect of the product from the ground up.</p>
-              <p>He currently lives and works in Los Angeles, California.</p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <div className="image">
-                <img src="https://source.unsplash.com/1600x900/?black" alt=""/>
-              </div>
-
-            </div>
-          </div>
-          <div class="row">
-            <div className="col-1 offset-1">
-              <p>He has worked and collaborated with industry leaders and global brands including Apple, Patagonia, MINI, Xero, and Sonos. Thomas’ work has been awarded and recognized globally and can be found in publications by Thames &amp; Hudson, Unit Editions, and Gestalten. He has lectured at numerous Universities around Australia and delivered keynotes at conferences including Semi-Permanent, The Design Conference and Creative Mornings.</p>
-            </div>
-            <div className="col-1">
-              <p>In 2011 he founded Made Publishers, a boutique publishing company with a focus on design and culture. Made Publishers has published over 20 editions of Process Journal, MADE and Nourished Journal, all of which can be found in stockists globally, including MoMA (New York) and The Design Museum (London).</p>
-              
-            </div>
-            <div className="col-1">
-              <p>In 2018 Thomas also launched Construct, a new digital product that creates responsive, web-based fee proposals. He spent over two years designing almost every aspect of the product from the ground up.</p>
-              <p>He currently lives and works in Los Angeles, California.</p>
-            </div>
-          </div>
-          <hr />
-          <header className="row">
-            <div className="col-3">
-              <h2>Thomas Williams is an Australian designer with over 12 years experience working across brand identity, publishing and digital products. </h2>
-            </div>
-            <div className="col-1">
-              <p>Digital design</p>
-            </div>
-          </header>
-          <hr />
-          <div class="row">
-            <div className="col-1">
-              <p>He has worked and collaborated with industry leaders and global brands including Apple, Patagonia, MINI, Xero, and Sonos. Thomas’ work has been awarded and recognized globally and can be found in publications by Thames &amp; Hudson, Unit Editions, and Gestalten. He has lectured at numerous Universities around Australia and delivered keynotes at conferences including Semi-Permanent, The Design Conference and Creative Mornings.</p>
-            </div>
-            <div className="col-1">
-              <p>In 2011 he founded Made Publishers, a boutique publishing company with a focus on design and culture. Made Publishers has published over 20 editions of Process Journal, MADE and Nourished Journal, all of which can be found in stockists globally, including MoMA (New York) and The Design Museum (London).</p>
-              
-            </div>
-            <div className="col-1">
-              <p>In 2018 Thomas also launched Construct, a new digital product that creates responsive, web-based fee proposals. He spent over two years designing almost every aspect of the product from the ground up.</p>
-              <p>He currently lives and works in Los Angeles, California.</p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <div className="image">
-                <img src="https://source.unsplash.com/1600x900/?black" alt=""/>
-              </div>
-
-            </div>
-          </div>
-          <div class="row">
-            <div className="col-1">
-              <p>He has worked and collaborated with industry leaders and global brands including Apple, Patagonia, MINI, Xero, and Sonos. Thomas’ work has been awarded and recognized globally and can be found in publications by Thames &amp; Hudson, Unit Editions, and Gestalten. He has lectured at numerous Universities around Australia and delivered keynotes at conferences including Semi-Permanent, The Design Conference and Creative Mornings.</p>
-            </div>
-            <div className="col-1">
-              <p>In 2011 he founded Made Publishers, a boutique publishing company with a focus on design and culture. Made Publishers has published over 20 editions of Process Journal, MADE and Nourished Journal, all of which can be found in stockists globally, including MoMA (New York) and The Design Museum (London).</p>
-              
-            </div>
-            <div className="col-1">
-              <p>In 2018 Thomas also launched Construct, a new digital product that creates responsive, web-based fee proposals. He spent over two years designing almost every aspect of the product from the ground up.</p>
-              <p>He currently lives and works in Los Angeles, California.</p>
-            </div>
-          </div>
-          <hr /> */}
         </div>
         
     );
